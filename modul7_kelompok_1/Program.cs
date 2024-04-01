22f7af7c13338a450e593a8309964c61c5dc26f1
Indra Mahesa <indramahesa128@gmail.com>
2024-04-01 07:16:51 +0700

    menambahkan class TeamMembers1302220067

--- modul7_kelompok_1/Program.cs
+++ modul7_kelompok_1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text.Json;
+using static DataMahasiswa1302220067;
+using static TeamMembers1302220067;
 
 internal class Program
 {
@@ -6,6 +8,11 @@ internal class Program
     {
         var data = new DataMahasiswa1302220067();
         data.ReadJSON();
+
+        Console.WriteLine();
+
+        var tes = new TeamMembers1302220067();
+        tes.ReadJSON();
     }
 }
 
@@ -41,5 +48,38 @@ public class DataMahasiswa1302220067
         Mahasiswa mahasiswa = JsonSerializer.Deserialize<Mahasiswa>(json, new JsonSerializerOptions { IncludeFields = true });
 
         Console.WriteLine("Nama saya {0} {1} berumur {2}, saya berkelamin {3} dan saya tinggal di {4}, {5}, {6}", mahasiswa?.firstName, mahasiswa?.lastName, mahasiswa?.age, mahasiswa?.gender, mahasiswa?.address.streetAddress, mahasiswa.address.city, mahasiswa.address.state);
+        Console.WriteLine("MataKuliah list:");
+        for (int i = 0; i < mahasiswa.courses.Length; i++)
+        {
+            Console.WriteLine("{0} {1}", mahasiswa.courses[i].code, mahasiswa.courses[i].name);
+        }
+    }
+}
+public class TeamMembers1302220067
+{
+    public class Members
+    {
+        public Member[] members;
+    }
+
+    public class Member
+    {
+        public String? firstName;
+        public String? lastName;
+        public String? gender;
+        public int? age;
+        public String? nim;
+    }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("../../../jurnal7_2_1302220067.json");
+        var members = JsonSerializer.Deserialize<Members>(json, new JsonSerializerOptions { IncludeFields = true });
+
+        Console.WriteLine("Team member list:");
+        for(int i = 0; i < members.members.Length; i++)
+        {
+            Console.WriteLine("{0} {1}-{2}", members.members[i].nim, members.members[i].firstName, members.members[i].lastName);
+        }
     }
 }
