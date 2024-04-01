3b9dd57e570e2b3da0c65c1b7900ef494dcec0b8
hsilasss <hsilassamban@gmail.com>
2024-04-01 07:32:30 +0700

    menambahkan class DataMahasiswa

---
+++ modul7_kelompok_1/DataMahasiswa1302220087.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class DataMahasiswa1302220087
+{
+	public class Nama {
+		public string first;
+		public string last;
+	}
+
+	public class Praktikan1302220087
+	{
+		public Nama nama;
+	}
+
+	public static void readJSON()
+	{
+		var djson = File.ReadAllText("../jurnal_7_1302220087.json");
+		var praktikan = JsonSerializer.Deserialize<Praktikan1302220087>(djson,new JsonSerializer.Options {IncludeFields = true});
+		Console.WriteLine("Nama {0} {1}",  praktikan.nama.first, praktikan.nama.last);
+
+	}
+}
